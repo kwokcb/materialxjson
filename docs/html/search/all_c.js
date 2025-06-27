e1252689303abcc4a5d7f37b13e78e082ac4d760
Bernard Kwok <kwokcb@gmail.com>
2025-06-27 09:19:39 -0400

    Bump version to 1.39.3

--- docs/html/search/all_c.js
+++ docs/html/search/all_c.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['package_0',['Package',['../index.html#autotoc_md1',1,'']]],
-  ['package_20setup_1',['Package Setup',['../md__d_1_2work_2materialx_2materialxjson_2_r_e_a_d_m_e.html#autotoc_md13',1,'']]]
+  ['package_20setup_1',['Package Setup',['../md__d_1_2_work_2materialx_2materialxjson_2_r_e_a_d_m_e.html#autotoc_md13',1,'']]]
 ];
--- docs/html/search/pages_2.js
+++ docs/html/search/pages_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['materialx_20json_20i_20o_0',['MaterialX JSON I/O',['../md__d_1_2work_2materialx_2materialxjson_2_r_e_a_d_m_e.html',1,'']]],
+  ['materialx_20json_20i_20o_0',['MaterialX JSON I/O',['../md__d_1_2_work_2materialx_2materialxjson_2_r_e_a_d_m_e.html',1,'']]],
   ['materialxjson_1',['MaterialXJSON',['../index.html',1,'']]]
 ];
